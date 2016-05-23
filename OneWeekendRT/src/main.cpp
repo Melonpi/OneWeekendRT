e1adc2f662fb43a908c030c38c75b9f7181ee4d6
nihil <nihil1943@gmail.com>
2016-05-23 21:20:12 +0800

    7.diffuse material

--- OneWeekendRT/src/main.cpp
+++ OneWeekendRT/src/main.cpp
@@ -14,12 +14,31 @@ const int SAMPLES = 100;
 const int RGBA = 4;
 typedef unsigned char byte;
 
+std::random_device rnd;
+std::mt19937_64 rne(rnd());
+std::uniform_real_distribution<real> rng(0, 1);
+
+real RNG()
+{
+	return rng(rne);
+}
+
+Vec3 randomInUnitSphere()
+{
+	Vec3 p;
+	do {
+		p = 2.0f * Vec3(RNG(), RNG(), RNG()) - Vec3(1, 1, 1);
+	} while (p.lengthSquared() >= 1.0);
+	return p;
+}
+
 Vec3 color(const Ray& ray, const Hitable& scene)
 {
 	HitInfo hit_info;
-	if (scene.hit(ray, 0, REAL_MAX, hit_info))
+	if (scene.hit(ray, REAL_EPS, REAL_MAX, hit_info))
 	{
-		return 0.5f * Vec3(hit_info.normal.x + 1, hit_info.normal.y + 1, hit_info.normal.z + 1);
+		Vec3 target = hit_info.p + hit_info.normal + randomInUnitSphere();
+		return 0.5f * color(Ray(hit_info.p, target - hit_info.p), scene);
 	}else{
 		Vec3 unit_direction = make_unit(ray.direction);
 		real t = 0.5f * (unit_direction.y + 1.0f);
@@ -39,9 +58,6 @@ int main(int argc, const char* argv[])
 	scene.add( std::unique_ptr<Sphere>(new Sphere(Vec3(0, 0, -1), 0.5)) );
 	scene.add( std::unique_ptr<Sphere>(new Sphere(Vec3(0, -100.5, -1), 100)) );
 	Camera camera;
-	std::random_device rnd;
-	std::mt19937_64 rne(rnd());
-	std::uniform_real_distribution<real> rng(0, 1);
 	for (int j = 0; j < h; ++j)
 	{
 		for (int i = 0; i < w; ++i)
@@ -49,13 +65,14 @@ int main(int argc, const char* argv[])
 			Vec3 col(0, 0, 0);
 			for (int n = 0; n < s; ++n)
 			{
-				real u = real(i + rng(rne)) / real(w);
-				real v = real(j + rng(rne)) / real(h);
+				real u = real(i + RNG()) / real(w);
+				real v = real(j + RNG()) / real(h);
 				Ray ray = camera.getRay(u, v);
 				Vec3 p = ray.pointAtParameter(2.0);
 				col += color(ray, scene);
 			}
 			col /= real(s);
+			col = { sqrt(col.r), sqrt(col.g), sqrt(col.b) };
 			data.get()[(h-j-1)*w*RGBA + i*RGBA    ] = int(col.r * 255.99);
 			data.get()[(h-j-1)*w*RGBA + i*RGBA + 1] = int(col.g * 255.99);
 			data.get()[(h-j-1)*w*RGBA + i*RGBA + 2] = int(col.b * 255.99);
