5dd9d99a6bb5189ecd2f20818464f3fe7dc0bb6f
melonpi <melonpi1943@gmail.com>
2016-06-02 18:52:40 +0800

    add tbb library.

--- OneWeekendRT/src/main.cpp
+++ OneWeekendRT/src/main.cpp
@@ -5,14 +5,18 @@
 #include "RNG.h"
 #include "Material.h"
 
+#define __TBB_NO_IMPLICIT_LINKAGE 1
+#include "tbb\tbb.h"
+#pragma warning(disable: 588)
+
 using namespace ow;
 
 #include <random>
 #include <iostream>
 
-const int WIDTH = 800;
-const int HEIGHT = 600;
-const int SAMPLES = 100;
+const int WIDTH = 400;
+const int HEIGHT = 300;
+const int SAMPLES = 1000;
 const int DEPTH_MAX = 40;
 const int RGBA = 4;
 typedef unsigned char byte;
@@ -53,8 +57,6 @@ std::unique_ptr<HitableList> make_random_scene()
 			{
 				if (material_seed < 0.8)//diffuse
 				{
-					//scene->add(std::make_unique<Sphere>(center, 0.2f, std::make_unique<Lambertian>(Vec3(RNG::rng()*RNG::rng(), RNG::rng()*RNG::rng(), RNG::rng()*RNG::rng()))));
-					//scene->add(std::make_unique<MovingSphere>(center, center + Vec3(0, 0.5 * RNG::rng(), 0), 0.0, 1.0, 0.2f, std::make_unique<Lambertian>(Vec3(RNG::rng()*RNG::rng(), RNG::rng()*RNG::rng(), RNG::rng()*RNG::rng()))));
 					std::unique_ptr<Texture> constant_texture = std::make_unique<ConstantTexture>(Vec3(RNG::rng()*RNG::rng(),RNG::rng()*RNG::rng(),RNG::rng()*RNG::rng()));
 					scene->add(std::make_unique<MovingSphere>(center, center + Vec3(0, 0.5 * RNG::rng(), 0), 0.0, 1.0, 0.2f, std::make_unique<Lambertian>(std::move(constant_texture))));
 				}
@@ -76,6 +78,11 @@ std::unique_ptr<HitableList> make_random_scene()
 	return std::move(scene);
 }
 
+inline void printProgress(int current, int total)
+{
+	std::cout << "pixels: " << current <<"/" << total <<"\n";
+}
+
 int main(int argc, const char* argv[])
 {
 	Vec3 v;
@@ -83,6 +90,7 @@ int main(int argc, const char* argv[])
 	int h = HEIGHT;
 	int s = SAMPLES;
 	std::unique_ptr<byte> data = std::unique_ptr<byte>(new byte[w * h * RGBA]);
+	tbb::atomic<int> rendered_pixels = 0;
 
 	std::unique_ptr<HitableList> scene = make_random_scene();
 
@@ -91,27 +99,29 @@ int main(int argc, const char* argv[])
 	real aperture = 0.0f;
 	real focus_dist = 10.0f;
 	Camera camera(lookfrom, lookat, Vec3(0, 1, 0), 20, real(w)/real(h), aperture, focus_dist, 0.0, 1.0);
-	for (int j = 0; j < h; ++j)
-	{
-		for (int i = 0; i < w; ++i)
+
+	tbb::parallel_for(size_t(0), size_t(w*h),
+		[w, h, s, &data, &scene, &camera, &rendered_pixels](size_t r) {
+		Vec3 col(0, 0, 0);
+		size_t j = r / w;
+		size_t i = r % w;
+		for (int n = 0; n < s; ++n)
 		{
-			std::system("cls");
-			std::cout << "rendering pixel x:(" << i+1 << "/" << w <<") y:(" << j+1 << "/" << h << ")\n";
-			Vec3 col(0, 0, 0);
-			for (int n = 0; n < s; ++n)
-			{
-				real u = real(i + RNG::rng()) / real(w);
-				real v = real(j + RNG::rng()) / real(h);
-				Ray ray = camera.getRay(u, v);
-				col += color(ray, *scene, 0);
-			}
-			col /= real(s);
-			col = { sqrt(col.r), sqrt(col.g), sqrt(col.b) };//gamma 2
-			data.get()[(h-j-1)*w*RGBA + i*RGBA    ] = int(col.r * 255.99);
-			data.get()[(h-j-1)*w*RGBA + i*RGBA + 1] = int(col.g * 255.99);
-			data.get()[(h-j-1)*w*RGBA + i*RGBA + 2] = int(col.b * 255.99);
+			real u = real(i + RNG::rng()) / real(w);
+			real v = real(j + RNG::rng()) / real(h);
+			Ray ray = camera.getRay(u, v);
+			col += color(ray, *scene, 0);
 		}
-	}
+		col /= real(s);
+		col = { sqrt(col.r), sqrt(col.g), sqrt(col.b) };//gamma 2
+		data.get()[(h-j-1)*w*RGBA + i*RGBA    ] = int(col.r * 255.99);
+		data.get()[(h-j-1)*w*RGBA + i*RGBA + 1] = int(col.g * 255.99);
+		data.get()[(h-j-1)*w*RGBA + i*RGBA + 2] = int(col.b * 255.99);
+		data.get()[(h-j-1)*w*RGBA + i*RGBA + 3] = 255;
+
+		rendered_pixels++;
+		printProgress(rendered_pixels, w*h);
+	});
 	if (stbi_write_png("output.png", WIDTH, HEIGHT, RGBA, data.get(), 0) == 0)
 	{
 		std::cout << "write png file failed.\n";
